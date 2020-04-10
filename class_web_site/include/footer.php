879201d3cbf33e1bf4487b22fcfec43e3e8e919e
Erik van Renselaar <erik@neit.edu>
2020-04-10 16:17:50 -0400

    Class Web Site

---
+++ class_web_site/include/footer.php
@@ -0,0 +1,10 @@
+<hr />          
+    <?php       
+        $file = basename($_SERVER['PHP_SELF']);
+        $mod_date=date("F d Y h:i:s A", filemtime($file));
+        echo "File last updated $mod_date ";
+    ?>
+ 
+</body>
+
+</html>
