8b39efeed6c909fe3e2c910a0ebfda9c9aabd045
Erik van Renselaar <erik@neit.edu>
2023-10-02 09:53:42 -0400

    Words of advice regarding learning

--- week1/01PHPTest.php
+++ week1/01PHPTest.php
@@ -8,8 +8,13 @@
 		<h1>SE 266 - Hello World</h1>
        
        <?php
-            echo "Welcome to PHP. PHP *really* rocks!";
+            echo "Welcome to PHP. </h1>";
         ?>
        
+       <h2>Words to live by</h2>
+       <ul>
+            <li>Learning is not a spectator sport</li>
+            <li>80% of the word "learn" is "earn"</li>
+       </ul>
     </body>
 </html>
